4d97044b25e6a2ff2163c6f2a35898502bceb2ce
Kanwar Singh <ks2591@gmail.com>
2021-12-02 19:54:15 +0800

    day1 code

---
+++ day1/day1.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const $file = fs.readFileSync('./day1/day1.input');
+
+const findIncreasingPoints = (input) => {
+  let count = 0;
+  let lastVal = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (i > 0 && lastVal < input[i]) {
+      count++;
+    }
+    lastVal = input[i];
+  }
+  return count;
+};
+
+// console.log($file.toString().split('\n'));
+
+console.log(
+  findIncreasingPoints(
+    $file
+      .toString()
+      .split('\n')
+      .map((n) => Number(n)),
+  ),
+);
